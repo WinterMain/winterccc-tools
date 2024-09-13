1c52ca2ad7c36c6525f9b657cf65c10e379f0b44
Winter Yang <winter.yang@vinotech.com>
2024-09-13 12:31:16 +0800

    Relase：20240913123116

---
+++ _nuxt/Cvn2hM3C.js
@@ -0,0 +1,1 @@
+import{bY as s}from"./CTJ3n5Ue.js";import{u}from"./Btng3dmt.js";function i(e,a){const{title:r,titleTemplate:o,...m}=e;return u({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const n=s({...t._flatMeta});return delete t._flatMeta,{...t,meta:n}}})}export{i as u};
