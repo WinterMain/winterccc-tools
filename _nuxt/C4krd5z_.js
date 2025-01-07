d1de55b723040d641dcd69d58672b0fe6b360b72
Winter Yang <winter.yang@vinotech.com>
2025-01-07 17:22:02 +0800

    Relase：20250107172202

---
+++ _nuxt/C4krd5z_.js
@@ -0,0 +1,1 @@
+import{u as t,_ as a}from"./Dy__Rov0.js";import{a as o,A as r,d as u,e as n,b as _,P as f}from"./DLp33f98.js";const l={class:"c-page-func"},m={class:"func-stage"},h={__name:"PageFunc",setup(d){const{currentFunc:e}=t();return(c,i)=>{const s=a;return o(),r("div",l,[u(s,{"current-func":n(e)},null,8,["current-func"]),_("div",m,[f(c.$slots,"default",{func:n(e)})])])}}};export{h as _};
---
+++ _nuxt/ScZoHvk5.js
@@ -0,0 +1,1 @@
+import{aj as s}from"./DLp33f98.js";const m=s("/images/empty.svg");export{m as _};
